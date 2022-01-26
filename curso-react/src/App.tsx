be30f3156809b92a8962c9bf3bcb081584942b64
Michel Amorim <michelmsa7799@gmail.com>
2022-01-26 00:29:59 -0300

    Conceitos de estado e imutabilidade

--- curso-react/src/App.tsx
+++ curso-react/src/App.tsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import Menu from "./components/Menu";
 
 function App() {
+  const [counter, setCounter] = useState(1);
+  const [name, setName] = useState("");
+
+  function handleButtonClick() {
+    setCounter(counter + 1);
+
+    setName(name);
+    console.log(counter);
+  }
+
   return (
     <>
       <Header title="pagina Inicial" />
+
       <h1>Hello Word</h1>
+
+      <p>{counter}</p>
+
+      <button onClick={handleButtonClick}>Incrementar</button>
+
       <Menu>
         <ul>
           <li>Home</li>
