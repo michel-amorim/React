c8ea2ab8d157a846a0368404563a00cf29ba4185
Michel Amorim <michelmsa7799@gmail.com>
2022-01-25 21:44:04 -0300

    criando componentes

--- curso-react/src/App.tsx
+++ curso-react/src/App.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Home from "./components/Home";
 
 function App() {
-  return <h1>Hello Word</h1>;
+  return (
+    <>
+      <Home />
+      <h1>Hello Word</h1>
+    </>
+  );
 }
 
 export default App;
