3bc98f77808906020e0862e6b6b8fece5b77e009
Michel Amorim <michelmsa7799@gmail.com>
2022-01-26 12:59:56 -0300

    pagina Home SPA REACT

--- curso-react/src/App.tsx
+++ curso-react/src/App.tsx
@@ -1,35 +1,12 @@
-import React, { useState } from "react";
-import Header from "./components/Header";
-import Menu from "./components/Menu";
+import React from "react";
+import "./App.css";
+
+import Routes from "./routes";
 
 function App() {
-  const [counter, setCounter] = useState(1);
-  const [name, setName] = useState("");
-
-  function handleButtonClick() {
-    setCounter(counter + 1);
-
-    setName(name);
-    console.log(counter);
-  }
-
   return (
     <>
-      <Header title="pagina Inicial" />
-
-      <h1>Hello Word</h1>
-
-      <p>{counter}</p>
-
-      <button onClick={handleButtonClick}>Incrementar</button>
-
-      <Menu>
-        <ul>
-          <li>Home</li>
-          <li>Contact</li>
-          <li>About</li>
-        </ul>
-      </Menu>
+      <Routes />
     </>
   );
 }
---
+++ curso-react/src/pages/CreateLocation/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { FiLogIn } from "react-icons/fi";
+
+const CreateLocation: React.FC = () => {
+  return <h1>Create Location</h1>;
+};
+
+export default CreateLocation;
