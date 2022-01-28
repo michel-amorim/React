5d230f9b77b384b0737814bfcd610a452e48c1f9
Michel Amorim <michelmsa7799@gmail.com>
2022-01-27 23:04:47 -0300

    Create Location SPA React

--- curso-react/src/pages/CreateLocation/index.tsx
+++ curso-react/src/pages/CreateLocation/index.tsx
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { Link, useHistory } from "react-router-dom";
 import { FiArrowLeft } from "react-icons/fi";
+import { Map, TileLayer, Marker } from "react-leaflet";
+import api from "../../services/api";
+
 import logo from "../../assets/logo.svg";
 import "../CreateLocation/style.css";
 
+interface Item {
+  id: number;
+  title: string;
+  image_url: string;
+}
+
 const CreateLocation: React.FC = () => {
+  const [items, setItems] = useState<Item[]>([]);
+
+  // função assíncrona
+  useEffect(() => {
+    api.get("items").then((response) => {
+      setItems(response.data);
+    });
+  }, []);
+
   return (
     <div id="page-create-location">
       <div className="content">
@@ -49,6 +67,14 @@ const CreateLocation: React.FC = () => {
               <span>Marque o endereço no mapa</span>
             </legend>
 
+            <Map center={[-23.0003709, -43.365895]} zoom={14}>
+              <TileLayer
+                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+              <Marker position={[-23.0003709, -43.365895]} />
+            </Map>
+
             <div className="field-group">
               <div className="field">
                 <label htmlFor="city">Cidade</label>
@@ -68,24 +94,11 @@ const CreateLocation: React.FC = () => {
               <span>Você pode marcar um ou mais ítens</span>
             </legend>
             <ul className="items-grid">
-              <li>
-                <img src="http://localhost:333/uploads/papel.svg" alt="Papel" />
-              </li>
-              <li className="selected">
-                <img src="http://localhost:333/uploads/papel.svg" alt="Papel" />
-              </li>
-              <li>
-                <img src="http://localhost:333/uploads/papel.svg" alt="Papel" />
-              </li>
-              <li>
-                <img src="http://localhost:333/uploads/papel.svg" alt="Papel" />
-              </li>
-              <li>
-                <img src="http://localhost:333/uploads/papel.svg" alt="Papel" />
-              </li>
-              <li>
-                <img src="http://localhost:333/uploads/papel.svg" alt="Papel" />
-              </li>
+              {items.map((item) => (
+                <li key={item.id}>
+                  <img src={item.image_url} alt={item.title} />
+                </li>
+              ))}
             </ul>
           </fieldset>
 
